c365ededa8b8205766bd56b24a475796799fb6bf
Dennis Gaturu <gaturugaturu60@gmail.com>
2024-07-11 16:47:08 +0300

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    // Attempting to reassign a constant will throw an error
+    leastFavoriteCustomer = 'another person';
+}
+
